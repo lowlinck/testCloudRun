2db6e84c093e4a39ca62cd695bf6f926c2aa9d70
smart-x <smartx.com.ua@gmail.com>
2024-10-03 17:41:16 -0600

    second commit

--- testCloudRun/index.php
+++ testCloudRun/index.php
@@ -1,2 +1,19 @@
-<?php
-echo "Hello, Cloud Run!";
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+<p class="main">
+    <?php
+    echo "Hello, Cloud Run!";
+    ?>
+
+</p>
+</body>
+</html>
